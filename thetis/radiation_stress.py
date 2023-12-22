5893628a49612d3b99dc93eda96a0f48de619ce5
fragkouan <fragkou.an@gmail.com>
2023-12-22 14:05:30 +0000

    Updated documentation

--- thetis/radiation_stress.py
+++ thetis/radiation_stress.py
@@ -43,7 +43,7 @@ class WaveEffectsOnCurrents(object):
 
 
     def update(self, grad_rad_stress=None, l_wave=None, dir_wave = None,
-                hs_wave=None, qb=None, c1=1.0):
+                h_wave=None, qb=None, c1=1.0):
         """
         Update the wave effects on currents
         """
@@ -56,45 +56,70 @@ class WaveEffectsOnCurrents(object):
                 "(WEOC) hasn't been implemented yet"
             raise SystemExit(message)
 
-        # Use the Mellor formulation to calculate the gradient of radiation
+        # Use the Mellor (2015) formulation to calculate the gradient of radiation
         # stress from waves
         else:
-            hs_wave.dat.data[:] = hs_wave.dat.data[:] * c1
+            # Update the radiation stress formulation by multiplying with the
+            # ramp coefficient
+            h_wave.dat.data[:] = h_wave.dat.data[:] * c1
             dir_wave.dat.data[:] = dir_wave.dat.data[:] * c1
             l_wave.dat.data[:] = l_wave.dat.data[:] * c1
             qb.dat.data[:] = qb.dat.data[:] * c1
 
             # Multiply with coeff
-            self.mellor(l_wave, dir_wave, hs_wave)
+            self.mellor(l_wave, dir_wave, h_wave)
 
         # Include wave roller effects
         if self.options.use_roller:
-            self.roller_effects(l_wave, dir_wave, hs_wave, qb)
+            self.roller_effects(l_wave, dir_wave, h_wave, qb)
 
 
     def swan_results(self, grad_rad_stress):
         """
-        Get the gradient
+        Not implemented yet. The plan is to use the already calculated SWAN's
+        radiation stress directly and not calculate it from scratch
+
+        Args:
+            grad_rad_stress:
+
+        Returns:
         """
+
+
         self.fields.rad_stress_2d.interpolate(Constant(1.)*grad_rad_stress)
 
+        return
 
-    def mellor(self, l_wave, dir_wave, hs_wave):
+
+    def mellor(self, l_wave, dir_wave, h_wave):
+
         """
         Use the formulation found in
-        Mellor, G. (2015), "A combined derivation of the integrated and vertic-
-        ally resolved, coupled wave-current equations", Journal of Physical Oc-
-        eanography 45(6), 1453-1463
-        S_{ij} = E * ( c_g/c * k_i*k_j/k**2 + \delta_ij * ( c_g/c - 0.5 ) )
-        where   E is the wave energy
-                c_g : the group velocity
-                c : the phase velocity
-                k : the wavenumber
-                \delta_ij : the Kronecker delta function (=1 when i=j, or =0
-                            otherwise)
-                i,j : the x- or y-direction
-                S_ij : the vertically integrated radiation stress
-        to calculate the gradient of radiation stress
+        Mellor, G. (2015), "A combined derivation of the integrated and \
+        vertically resolved, coupled wave-current equations", Journal of \
+        Physical Oceanography 45(6), 1453-1463
+            $ S_{ij} = E * ( c_g/c * k_i*k_j/k^2 + \delta_ij*(c_g/c - 0.5) ) $
+        where
+            E is the wave energy;
+            c_g : the group velocity;
+            c : the phase velocity;
+            k : the wavenumber;
+            \delta_ij : the Kronecker delta function (=1 when i=j, else =0);
+            i,j : the x- or y-direction;
+            S_ij : the vertically integrated radiation stress
+        to calculate the gradient of radiation stress and update the relevant
+        Thetis field
+
+        Note:
+            It updates the self.fields.rad_stress_2d
+
+        Args:
+            l_wave: Wavelength [m]
+            dir_wave : Wave direction [o]
+            h_wave : Wave height [m]
+
+        Returns:
+            None
         """
         # Define limiters
         # Minimum wavelength [m]
@@ -122,24 +147,29 @@ class WaveEffectsOnCurrents(object):
 
         #Calculate the wave energy
         if self.options.use_monochromatic:
-            # The Hs from SWAN is the actual wave height, thus E = 1/8*rho*g*Hs^2
-            # From Kudale & Bhalerao (2015) "Design wave height should be average
-            # of the highest 1/10 of the waves, i.e. H_(1/10), instead of Hs
-            # while representing waves by monochromatic wave trains
-            # H_(1/10) = 1.27*Hs
-            energy = Function(self.P1_2d).interpolate(Constant(1/8*1.27**2)*self.rho0*self.g_grav*hs_wave**2)
+            # From Kudale & Bhalerao (2015) : "Design wave height should be
+            # average of the highest 1/10 of the waves, i.e. H_(1/10), instead
+            # of just H while representing waves by monochromatic wave trains
+            # H_(1/10) = 1.27*H
+            energy = Function(self.P1_2d).interpolate(
+                Constant(1/8*1.27**2)*self.rho0*self.g_grav*h_wave**2
+            )
         else:
             energy = Function(self.P1_2d).interpolate(
-                Constant( 1 / 8 ) * self.rho0 * self.g_grav * hs_wave ** 2
+                Constant( 1 / 8 ) * self.rho0 * self.g_grav * h_wave ** 2
             )
 
         # Calculate total depth
         h_total = self.depth.get_total_depth(self.fields.elev_2d)
         #Calculate the (wavenumber)*(total_depth) and apply limiter
-        kh = Function(self.P1_2d).interpolate(Min(k*h_total+Constant(10**(-14)), kh_max))
+        kh = Function(self.P1_2d).interpolate(
+            Min(k*h_total+Constant(10**(-14)), kh_max)
+        )
 
         #Calculate (group velocity)/(phase velocity)
-        n = Function(self.P1_2d).interpolate(Constant(0.5)+kh/sinh(Constant(2.)*kh))
+        n = Function(self.P1_2d).interpolate(
+            Constant(0.5)+kh/sinh(Constant(2.)*kh)
+        )
 
         #Calculate radiation stress
         sxx = Function(self.P1_2d).interpolate(
@@ -160,17 +190,38 @@ class WaveEffectsOnCurrents(object):
         s_x = Function(self.P1_2d).interpolate(-sxx_dx-sxy_dy)
         s_y = Function(self.P1_2d).interpolate(-syy_dy-sxy_dx)
 
-        # # Multiply with the coefficient
-        # s_x.dat.data[:] = s_x.dat.data[:] * c1
-        # s_y.dat.data[:] = s_y.dat.data[:] * c1
-
         # Update field
         self.fields.rad_stress_2d.interpolate(as_vector([s_x, s_y]))
         
         return
 
 
-    def roller_effects(self, l_wave, dir_wave, hs_wave, qb):
+    def roller_effects(self, l_wave, dir_wave, h_wave, qb):
+        """
+        Calculate the gradient of wave roller effects according to Svendsen \
+        (1984) and Reniers & Battjes (1987):
+            $ R_{ij} = 2 * E_r * k_i*k_j/k^2 $
+        where
+            $ E_r = \\rho * g * A_r * \sin{\phi} $ is the energy due to wave \
+            roller interface calculate according to Duncan (1981);
+            $ A_r = 0.9 * H^2 $ is the roller area (Svendsen 1984);
+            $ \phi$ is the roller angle with $ \tan{phi} ~ 0.1 $ (Reniers & \
+            Battjes 1997; Martins et al. 2018)
+            $ k $ is the wavenumber;
+            $ i, j$ are the $x-$ and $y-$direction respectively
+
+        Note:
+            The self.fields.roller_2d is updated
+
+        Args:
+            l_wave : Wavelength [m]
+            dir_wave : Wave direction [o]
+            h_wave : Wave height [m]
+            qb : Percentage of wave breking [-]
+
+        Returns:
+            None
+        """
         # Define limiters
         # Minimum wavelength [m]
         l_min = Constant(1.)
@@ -197,13 +248,15 @@ class WaveEffectsOnCurrents(object):
 
         # Define constants
         sinb = 0.1
+
         if self.options.use_monochromatic:
-            # Calculate roller wave area according to Svendsen 1984 (see Martins et al. 2018)
+            # Calculate roller wave area according to Svendsen 1984 (see \
+            # Martins et al. 2018)
             A = Function(self.P1_2d).interpolate(
-                Constant(0.9 * 1.27 ** 2) * hs_wave ** 2 * qb)
+                Constant(0.9 * 1.27 ** 2) * h_wave ** 2 * qb)
         else:
             A = Function(self.P1_2d).interpolate(
-                Constant(0.9) * (hs_wave) ** 2 * qb
+                Constant(0.9) * (h_wave) ** 2 * qb
             )
 
         # Calculate the roller wave energy
@@ -221,9 +274,15 @@ class WaveEffectsOnCurrents(object):
         n = Function(self.P1_2d).interpolate(
             Constant(0.5) + kh / sinh(Constant(2.) * kh))
 
-        sxx = Function(self.P1_2d).interpolate(Constant(2) * roller_en * kx ** 2 / k ** 2)
-        sxy = Function(self.P1_2d).interpolate(Constant(2) * roller_en * kx * ky / k ** 2)
-        syy = Function(self.P1_2d).interpolate(Constant(2) * roller_en * ky ** 2 / k ** 2)
+        sxx = Function(self.P1_2d).interpolate(
+            Constant(2) * roller_en * kx ** 2 / k ** 2
+        )
+        sxy = Function(self.P1_2d).interpolate(
+            Constant(2) * roller_en * kx * ky / k ** 2
+        )
+        syy = Function(self.P1_2d).interpolate(
+            Constant(2) * roller_en * ky ** 2 / k ** 2
+        )
 
         # Calculate gradient of radiation stress
         sxx_dx = Function(self.P1_2d).interpolate(sxx.dx(0))
@@ -237,4 +296,6 @@ class WaveEffectsOnCurrents(object):
 
         # Update field
         self.fields.roller_2d.interpolate(as_vector([s_x, s_y]))
+
+        return
 
